83f2eeae26e334b8eab8986cc6b3c32cf6ffa0ca
saijeevanballa <saijeevanballa@gmail.com>
2020-04-10 14:20:10 +0530

    2 files modified, 4 added

---
+++ src/app/components/counter/counter.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-counter",
+  templateUrl: "./counter.component.html",
+  styleUrls: ["./counter.component.css"]
+})
+export class CounterComponent {
+  public count = 0
+  constructor() {}
+  onAdd = () => ++this.count;
+  onSub = () => --this.count;
+}
